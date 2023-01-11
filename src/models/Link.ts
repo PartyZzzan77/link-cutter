beb67fa176234f1695f2f68e0e7eb42eded3a8c7
Mitya <partyzan327@gmail.com>
2023-01-11 21:42:34 +0300

    feat: describe the link scheme in the DB

---
+++ src/models/Link.ts
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose'
+
+const LinkSchema = new Schema({
+	cipher: { type: String, unique: true },
+	source: { type: String },
+	timestamp: { type: Date, default: Date.now() }
+})
+
+export const Link = model('Link', LinkSchema)
